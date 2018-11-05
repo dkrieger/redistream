0fc1444f9fd2aeab99697113383e41cc84e2a38b
Doug Krieger <doug@cashortrade.org>
2018-11-05 13:33:15 -0500

    add GPL license

--- redistream.go
+++ redistream.go
@@ -1,3 +1,20 @@
+/*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright (C) Doug Krieger <doug@debutdigital.com>, 2018
+ */
+
 // redistream provides higher-level client-side stream operations on top of
 // redis stream semantics. Additionally, it provides wrappers on top of
 // lower-level redis stream commands to do things like injecting defaults. By
